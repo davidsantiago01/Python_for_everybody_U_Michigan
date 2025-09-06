9e3beb1b3c553419cc33ea8a6c529afebdda1745
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-05 23:20:02 -0600

    Create XSD_Data_Types.py

---
+++ Course_3Using_Python_to_Access_Web_Data/XSD_Data_Types.py
@@ -0,0 +1,11 @@
+<xs:element name ="customer" type="xs.string"/>
+<xs:element name ="start" type="xs.date"/>
+<xs:element name ="stardate" type="xs.dateTime"/> 
+<xs:element name ="prize" type="xs.decimal"/>
+<xs:element name ="weeks" type="xs.integer"/>
+
+<customer>John</customer>
+<start>2002-09-24</start> #stardard date use on internet, starting with year, month, day
+<startdate>2002-05-30T09:30:10Z</startdate> #Z =timezone
+<prize>999.50</prize>
+<weeks>30</weeks>
