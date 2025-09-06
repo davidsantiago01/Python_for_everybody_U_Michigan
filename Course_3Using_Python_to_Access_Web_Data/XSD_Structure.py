a7190e4190a9ced9ea2c5795ea1ba6b617317159
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-05 23:01:05 -0600

    Update XSD_Structure.py

--- Course_3Using_Python_to_Access_Web_Data/XSD_Structure.py
+++ Course_3Using_Python_to_Access_Web_Data/XSD_Structure.py
@@ -1,1 +1,17 @@
+#XSD Structure
 
+xs:element naranja
+xs:sequence verde
+xs:complexType amarillo
+
+ <person> amarillo
+  <lastname> Severance </lastname> naranja
+  <age> 17 </age> naranja
+  <dateborn>2001-04-17</dateborn> naranja
+ <person> amarillo
+
+ <xs:complexType name="person">
+   <xs:sequence>
+      <xs:element name="lastname" type="xs:string"/>
+      <xs:element name="age" type="xs:integer"/>
+      <xs:element name="dateborn" type="xs:date"/>
