d4afc7702c7bb5bc65b65bb5e62701d44ed03469
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-05 22:23:55 -0600

    Update XML_Tree.py

--- Course_3Using_Python_to_Access_Web_Data/XML_Tree.py
+++ Course_3Using_Python_to_Access_Web_Data/XML_Tree.py
@@ -1,11 +1,31 @@
 #Example of XML as a Tree
 
-<a>
-  <b> X </b>
+<a> #open tag
+  <b> X </b> #children of a
+  <c> #children of a
+    <d> Y </d> #children of c
+    <e> Z </e> #children of c
+  </c> #close c
+</a> #close tag
+
+ 
+# XML and Attributes
+ <a>
+  <b W="5"> X </b> #5 attribute of W, X = text node
   <c>
     <d> Y </d>
     <e> Z </e>
-  <c>
-<a>
+  </c>
+</a>
 
- 
+
+#XML as Paths
+<a> #open tag
+  <b> X </b> #children of a  
+  <c> #children of a                  /a/b   X
+    <d> Y </d> #children of c         /a/c/d Y
+    <e> Z </e> #children of c         /a/c/e Z 
+  </c> #close c
+</a> #close tag
+
+
