e1bab1a74ce628f5bdd4ce1c4bc72121f33f2b01
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-06 23:41:16 -0600

    Create JSON_Example_2.py

---
+++ Course_3Using_Python_to_Access_Web_Data/JSON_Example_2.py
@@ -0,0 +1,27 @@
+import json
+#Square Brackets menas a list, so ID 001 and ID 009 is are dictionaries = a list of two dictionaries
+input = '''[  
+{"id" : "001",
+"x" : "2",
+"name" : "Chuck"
+},
+{"id" : "009",
+"x" : "7",
+"name" : "Chuck"
+}
+]'''
+
+info = json.loads (input)
+print ("User count:" , len(info))
+for item in info:
+  print ("Name", item["name"])
+  print ("Id", item["id"])
+  print ("Attribute", item["x"])
+
+User count: 2
+Name Chuck
+Id 001
+Attribute 2
+Name Chuck
+Id 009
+Attribute 7
