2e52fc39c38bf93670065d332c58ebe632a1f33d
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-08 00:25:49 -0600

    Update Class_and_Object.py

--- Course_4_Using_Databases_with_Python/Class_and_Object.py
+++ Course_4_Using_Databases_with_Python/Class_and_Object.py
@@ -20,3 +20,16 @@ an.party()
 So far 1
 So far 2
 So far 3   
+
+
+# We can give dir() to find teh capabilities of our newly class created
+
+print ("Type", type(an))
+print ("Dir", dir(an))
+print ("X", an.x)
+print ("Type", type (an.party))
+
+Type <class '__main__.PartyAnimal'>
+Dir ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__firstlineno__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__static_attributes__', '__str__', '__subclasshook__', '__weakref__', 'party', 'x']
+X 0
+Type <class 'method'>
