d805d3a45ffce5a4a665dc0ac53c6c673e3db3e0
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-08 00:04:10 -0600

    Update Object_Oriented_Definition&Terminology.py

--- Course_4_Using_Databases_with_Python/Object_Oriented_Definition&Terminology.py
+++ Course_4_Using_Databases_with_Python/Object_Oriented_Definition&Terminology.py
@@ -1,1 +1,45 @@
 
+x = "abc"
+type (x)
+Class 'str' 
+type (2.5)
+Class'integer'
+type (2)
+Class 'Float'
+y = list()
+type (y)
+Class 'List?'
+z=dict()
+type (z)
+Class 'Dict'
+
+# the variable x is the object or instance
+
+#
+dir (x)
+['__add__',
+ '__class__',
+ '__contains__',
+ '__delattr__',
+ '__dir__',
+ '__doc__',
+ '__eq__',
+ '__format__',
+ '__ge__',
+ '__getattribute__',
+ '__getitem__',
+ '__getnewargs__',
+ '__getstate__',
+ '__gt__',
+ '__hash__',
+ '__init__',
+ '__init_subclass__',
+ '__iter__',
+ '__le__',
+ '__len__',
+ '__lt__',
+ '__mod__',
+ '__mul__',
+ '__ne__',
+ '__new__',
+
