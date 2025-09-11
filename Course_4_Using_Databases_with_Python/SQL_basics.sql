71fa3a7a6d61877a398f53e30bf68332e31e55f9
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-10 23:40:22 -0600

    Update SQL_basics.sql

--- Course_4_Using_Databases_with_Python/SQL_basics.sql
+++ Course_4_Using_Databases_with_Python/SQL_basics.sql
@@ -1,6 +1,10 @@
-#SQL Insert 
+-- Select Information
+select * from users
+select name from users
+
+-- SQL Insert: Insert statements inserts a row into a table
 insert into users (name, email)
 values ("Pete", "Pete@umichigan.com";
 
-
+-- 
 
