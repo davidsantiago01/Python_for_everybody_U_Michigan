5234652346b3d6ec063c5a508062a5d17c01d38a
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-10 23:53:22 -0600

    Update SQL_basics.sql

--- Course_4_Using_Databases_with_Python/SQL_basics.sql
+++ Course_4_Using_Databases_with_Python/SQL_basics.sql
@@ -1,4 +1,4 @@
--- Select Information
+-- Select Information: select statement retrieves a group of records - youcan either retrieve all the records or a subset of the records with a WHERE clause
 select * from users
 select name from users
 
@@ -12,4 +12,6 @@ delete from users where email="Pete@umichigan.com"
 -- SQL Update: allows the updating of a field with a where clause
 update users Set name = "Charles" where email = "csev@umichigan.com"
 
-
+-- Sorting with ORDER BY: you can add an order by clause to select statements to get the results sorted in ascending or descending order
+select * from users order by email
+select * from users order by name 
