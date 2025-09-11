62a1a3c5ab315bcfcca592a3c2bbeb1ef395981f
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-10 23:48:37 -0600

    Update SQL_basics.sql

--- Course_4_Using_Databases_with_Python/SQL_basics.sql
+++ Course_4_Using_Databases_with_Python/SQL_basics.sql
@@ -6,5 +6,10 @@ select name from users
 insert into users (name, email)
 values ("Pete", "Pete@umichigan.com";
 
--- 
+-- SQL Delete: Deletes a row in a table based on a selection critera
+delete from users where email="Pete@umichigan.com"
+
+-- SQL Update: allows the updating of a field with a where clause
+update users Set name = "Charles" where email = "csev@umichigan.com"
+
 
