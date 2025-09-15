f594207206f3bd6e91b344db6875a3b2d2ea6b21
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-09-14 22:49:45 -0600

    Update Many_to_many_1.sql

--- Course_4_Using_Databases_with_Python/Many_to_many_1.sql
+++ Course_4_Using_Databases_with_Python/Many_to_many_1.sql
@@ -26,4 +26,31 @@ INSERT INTO Course (title) VALUES ("Python");
 INSERT INTO Course (title) VALUES ("SQL");
 INSERT INTO Course (title) VALUES ("PHP");
 
+-- Insert memberships
+Insert INTO Member (user_id, course_id , role) VALUES (1, 1, 1);
+Insert INTO Member (user_id, course_id , role) VALUES (2, 1, 0);
+Insert INTO Member (user_id, course_id , role) VALUES (3, 1, 0);
+
+Insert INTO Member (user_id, course_id , role) VALUES (1, 2, 0);
+Insert INTO Member (user_id, course_id , role) VALUES (2, 2, 1);
+
+Insert INTO Member (user_id, course_id , role) VALUES (2, 3, 1);
+Insert INTO Member (user_id, course_id , role) VALUES (3, 3, 0);
+
+SELECT User.name, Member.role, Course.title
+FROM User JOIN Member JOIN Course
+ON Member.user_id = User.id AND Member.course_id = Course.id
+ORDER BY Course.title, Member.role DESC, User.name
+--Ed	1	PHP
+--Sue	0	PHP
+--Jane	1	Python
+--Ed	0	Python
+--Sue	0	Python
+--Ed	1	SQL
+--Jane	0	SQL
+
+
+
+
+
 
